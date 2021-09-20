141a985a2e9377ee99e834a518908fea4e303408
AsilM <asil9802mum@gmail.com>
2021-09-20 19:00:44 +0500

    firebase config changed to mine

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,13 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  apiKey: "AIzaSyD0HSeei8joHvog92SQOMBuh7vei-dIrsg",
+  authDomain: "crwn-signin.firebaseapp.com",
+  projectId: "crwn-signin",
+  storageBucket: "crwn-signin.appspot.com",
+  messagingSenderId: "247292346257",
+  appId: "1:247292346257:web:24ffae5ddfcc0217122430",
+  measurementId: "G-3SR2L6TXLG"
 };
 
 firebase.initializeApp(config);
