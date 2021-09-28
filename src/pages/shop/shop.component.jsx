3b56a69f615a26d2a484897858e3c233ab561378
AsilM <asil9802mum@gmail.com>
2021-09-28 19:04:21 +0500

    redux-saga implemented in shop.component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-import { fetchCollectionsStartAsync} from '../../redux/shop/shop.actions';
+import { fetchCollectionsStart} from '../../redux/shop/shop.actions';
 
 import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
 import CollectionPageContainer from '../collection/collection.container';
 
 class ShopPage extends React.Component {
   componentDidMount() {
-    const { fetchCollectionsStartAsync} = this.props;
-    fetchCollectionsStartAsync()
+    const { fetchCollectionsStart} = this.props;
+    fetchCollectionsStart()
   }
 
   render() {
@@ -32,7 +32,7 @@ class ShopPage extends React.Component {
 }
 
 const mapDispatchToProps = dispatch => ({
-  fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync())
+  fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
 });
 
 export default connect(
