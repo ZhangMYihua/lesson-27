4646470ebcf3957b9edcc1fc8c9485d12e9feed4
AsilM <asil9802mum@gmail.com>
2021-10-02 00:05:43 +0500

    react hooks's useStae and useEffect partly implemented into the project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
@@ -15,19 +15,12 @@ import Header from "./components/header/header.component";
 import { selectCurrentUser } from "./redux/user/user.selectors";
 import { checkUserSession } from "./redux/user/user.actions";
 
-class App extends React.Component {
-  unsubscribeFromAuth = null;
-
-  componentDidMount() {
-    const {checkUserSession} = this.props;
-    checkUserSession();
-  }
-
-  componentWillUnmount() {
-    this.unsubscribeFromAuth();
-  }
-
-  render() {
+const App = ({checkUserSession, currentUser}) => {
+  
+  useEffect(() => {
+    checkUserSession()
+  }, [checkUserSession])
+  
     return (
       <div>
         <Header />
@@ -50,7 +43,7 @@ class App extends React.Component {
       </div>
     );
   }
-}
+
 
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
