a2016189c4e90499907804e16c8c99cd920bf8dc
AsilM <asil9802mum@gmail.com>
2021-10-10 21:20:46 +0500

    hooks implemented in App.js,  cart-dropdown and collections components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, {useEffect} from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
-import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
+import { useSelector, useDispatch} from "react-redux";
 
 import "./App.css";
 
@@ -15,11 +14,13 @@ import Header from "./components/header/header.component";
 import { selectCurrentUser } from "./redux/user/user.selectors";
 import { checkUserSession } from "./redux/user/user.actions";
 
-const App = ({checkUserSession, currentUser}) => {
-  
+const App = () => {
+  const currentUser = useSelector(selectCurrentUser)
+  const dispatch = useDispatch();
+
   useEffect(() => {
-    checkUserSession()
-  }, [checkUserSession])
+    dispatch(checkUserSession())
+  }, [dispatch])
   
     return (
       <div>
@@ -45,14 +46,4 @@ const App = ({checkUserSession, currentUser}) => {
   }
 
 
-const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser,
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  checkUserSession: () => {
-    dispatch(checkUserSession());
-  },
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
